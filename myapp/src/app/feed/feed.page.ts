50db97abd0b2dc87bfe53346549f05999524206c
Heitor Machado <heitorsbz@gmail.com>
2021-08-30 23:01:30 -0300

    Atualizacao

--- myapp/src/app/feed/feed.page.ts
+++ myapp/src/app/feed/feed.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedPage implements OnInit {
 
+  public user_name: string = "Milena Ferreira do Nascimento";
+
   constructor() { }
+
+  public soma(num1:number, num2:number): void{
+    alert(num1 + num2);
+  }
 
   ngOnInit() {
   }
 
 }
+
