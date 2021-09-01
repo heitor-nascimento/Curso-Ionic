2b51a5ae3e5ef50953288118e10817b46e0d4eb9
Heitor Machado <heitorsbz@gmail.com.br>
2021-09-01 01:03:05 -0300

    Ajustes no css da feed_page

--- myapp/src/app/feed/feed.page.ts
+++ myapp/src/app/feed/feed.page.ts
@@ -7,15 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedPage implements OnInit {
 
-  public user_name: string = "Milena Ferreira do Nascimento";
+  public user_name: string = "Heitor Machado Nascimento";
 
   constructor() { }
-
+/*
   public soma(num1:number, num2:number): void{
     alert(num1 + num2);
   }
-
+*/
   ngOnInit() {
+    alert("Teste");
   }
 
 }
